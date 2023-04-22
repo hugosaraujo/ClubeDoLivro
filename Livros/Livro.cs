7106fdd427fad52cb39d132c8cbcd417a7feee27
Hugo de Araujo <hugo.saraujo@tutanota.com>
2023-04-22 12:50:46 -0300

    Adicionar arquivos de projeto.

---
+++ Livros/Livro.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubeDoLivro.Livros
+{
+    public class Livro
+    {
+        public string Titulo { get; private set; }
+        public string Autor { get; set; }
+        public string Isbn { get; private set; }
+        public int AnoPublicacao { get; set; }
+        public string Categoria { get; set; }
+        public int QtdPaginas { get; set; }
+        public string PaisPublicacao { get; set; }
+
+        public Livro(string titulo, string isbn)
+        {
+            this.Titulo = titulo; 
+            this.Isbn = isbn; 
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿using ClubeDoLivro.Livros;
+
+Livro ceo = new Livro("Cândido ou otimismo", "91891808");
+
+Console.WriteLine(ceo.Titulo);
